c3841ccb3e6353da34876e0a1593e545a92be7f7
cuba <ezeokecc@gmail.com>
2019-08-03 22:32:01 +0100

    i have now reviewed 16 array methods. May the forces be with me

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,4 +1,4 @@
-// SPLICE METHOD
+// 1. SPLICE METHOD
 // let arr = ["I", "study", "Javascript", "right", "now"];
 
 //removing 2 items from index 1 and adding other items
@@ -13,7 +13,7 @@
 //removing or adding from the back
 // arr.splice(-3, 2, "array", "methods");
 
-// SLICE METHOD
+// 2. SLICE METHOD
 // arr.slice(start, end)-copies from first start to before end index parameter
 
 // let arr = ["t", "e", "s", "t"];
@@ -24,21 +24,21 @@
 //slice can aswell use a negative value for both start and end
 // console.log(arr.slice(-3));
 
-//CONCAT METHOD
+// 3. CONCAT METHOD
 // let arr = [1, 2];
 // console.log(arr.concat([3, 4], 5));
 
-//--------SEARCHING IN ARRAY---------
+//--------METHODS FOR SEARCHING AN ARRAY---------
 
-//INDEXOF - returns -1 if the item doesnt exist
+// 4. INDEXOF - returns -1 if the item doesnt exist
 // let arr = [false, 1, "baby"];
 // console.log(arr.indexOf(false));
 
-//LASTINDEXOF
+// 5. LASTINDEXOF
 // let arr = [false, 1, "baby"];
 // console.log(arr.lastIndexOf(1));
 
-//INCLUDES METHOD
+// 6. INCLUDES METHOD
 // let arr = [false, 1, "baby"];
 // console.log(arr.includes(1));
 
@@ -47,14 +47,64 @@
 // console.log(arr.includes(NaN));
 // console.log(arr.indexOf(NaN));
 
-//FIND METHOD
-let users = [
-  { id: 1, name: "John" },
-  { id: 2, name: "Pete" },
-  { id: 3, name: "Mary" }
-];
+// 7. FIND METHOD - looks for a single(first) element that makes the function return true
+// let users = [
+//   { id: 1, name: "John" },
+//   { id: 1, name: "Johnnie" },
+//   { id: 2, name: "Pete" },
+//   { id: 3, name: "Mary" }
+// ];
 
-let user = users.find(item => item.id == 1 && item.name == "John");
+// let user = users.find(item => item.id == 1);
 
-console.log(user);
-console.log(user.name);
+// console.log(user);
+// console.log(user.name);
+
+// 8. FILTER METHOD - returns all the elements that satisfy the condition
+// let users = [
+//   { id: 1, name: "John" },
+//   { id: 2, name: "Pete" },
+//   { id: 3, name: "Mary" }
+// ];
+
+// let userFilter = users.filter(item => item.id > 1);
+// console.log(userFilter);
+
+//---------METHODS FOR TRANSFORMING AN ARRAY----------
+// 9. MAP METHOD
+// let lengths = ["Bilbo", "Gandalf", "Nazgul"];
+// let numLength = lengths.map(item => item.length);
+// console.log(numLength);
+
+// 10. SORT METHOD- it sorts items in an array like a string. it can be optimized though
+// let arr = [31, 2, 0.3, 4, 5];
+// let arrSort = arr.sort((a, b) => a - b);
+// console.log(arrSort);
+
+// 11. REVERSE METHOD
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.reverse());
+// console.log(arr);
+
+// 12. SPLIT METHOD
+// let str = "test";
+// let splitStr = str.split("");
+// console.log(splitStr);
+
+// 13. JOIN METHOD
+// let str = ["t", "e", "s", "t"];
+// strJoin = str.join("");
+// console.log(strJoin);
+
+// 14. REDUCE METHOD
+// let arr = [1, 2, 3, 4, 5];
+// let result = arr.reduce((sum, current) => sum + current, 10);
+// console.log(result);
+
+// 15. FOREACH METHOD
+// let arr = ["Bilbo", "Gandalf", "Nazgul"];
+// arr.forEach(item => console.log(`${item} is a strange name`));
+
+//ARRAY.isARRAY METHOD - returns true if value is an array
+// console.log(Array.isArray([]));
+// console.log(Array.isArray({}));
